d37cb2f576ea31644ba7a97ad4e1463d5b152427
Alamgir Khan <alamgir.inu96@gmail.com>
2026-02-18 13:55:56 +0300

    pointing railwy database

--- src/server.js
+++ src/server.js
@@ -3,3 +3,5 @@ import env from "./config/env.js";
 const PORT = env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in ${env.NODE_ENV} mode on port ${PORT}`));
 export default app;
+
+console.log("DB_URL:", process.env.DATABASE_URL);
