7a688ddf6c5a6554a6cc51895b1a79bb763fb2bf
Alamgir Khan <alamgir.inu96@gmail.com>
2026-02-26 08:22:41 +0300

    prisma issue

--- src/utils/prismaClient.js
+++ src/utils/prismaClient.js
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
     datasources: {
-        db: process.env.DATABASE_URL,
+        db: {
+            url: process.env.DATABASE_URL,
+        },
     },
 });
 export default prisma;
